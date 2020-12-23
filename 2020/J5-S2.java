189cb1fcbd6d3b177d2638294246d6aaa98350a4
Sophie Zhang <65544037+sophie-zhangg@users.noreply.github.com>
2020-12-23 17:57:10 -0500

    Deleted unnecessary method

--- 2020/J5-S2.java
+++ 2020/J5-S2.java
@@ -4,20 +4,24 @@ import java.io.InputStreamReader;
 import java.io.IOException;
 import java.util.LinkedList;
 
-public class S220 {
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.LinkedList;
+
+public class Main {
 // M is rows, N is columns, new coordinates must multiply to number inside the old cell
 	// Start at (1,1), end is (M,N)
-	
 	static class Node {
         int cell;
         int product;
-
-        public Node(int r, int c, int cell) {
-            super();
-            this.cell = cell;
-            this.product = r * c;
-        }
-    }
+		
+		public Node(int r, int c, int cell) {
+		    super();
+		    this.cell = cell;
+		    this.product = r * c;
+		}
+	}
 	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -34,12 +38,9 @@ public class S220 {
 			}
 			// Finished loading input into 2D array
 		}
-		System.out.println(findPathFromEnd(room, room.getLast(), room.getFirst()));
-		br.close();
-	}
-	
-	private static String findPathFromEnd(LinkedList<Node> room, Node source, Node destination) {
-		// BFS Algorithm: Write a queue, if not the final element put all its children behind in the queue
+		Node source = room.getLast();
+		Node destination = room.getFirst();
+		// Write a queue, if not the final element put all its children behind in the queue
 		LinkedList<Node> queue = new LinkedList<Node>();
 		queue.add(source);
 		boolean notDone;
@@ -48,7 +49,8 @@ public class S220 {
 			notDone = false;
 			node = queue.remove(); //removes head of linkedlist 
 			if (node.product == destination.product) {
-				return "yes";
+				System.out.println("yes");
+				return;
 			}
 			room.remove(node);
 			for (Node cell: room) {
@@ -61,7 +63,8 @@ public class S220 {
 				continue;
 			}
 		}
-		return "no";
+		System.out.println("no");
+		br.close();
 	}
-	
 }
+
