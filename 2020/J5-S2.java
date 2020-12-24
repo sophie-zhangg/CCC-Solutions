5ae6c7498ca2e55abd7448fec548ca4a8f6b02ba
Sophie Zhang <65544037+sophie-zhangg@users.noreply.github.com>
2020-12-24 17:25:04 -0500

    Update J5-S2.java

--- 2020/J5-S2.java
+++ 2020/J5-S2.java
@@ -1,61 +1,76 @@
-// Only recieves 11/15, TLE for rest of test cases
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.LinkedList;
+import java.util.StringTokenizer;
 
-public class Main {
+public class S220 {
 // M is rows, N is columns, new coordinates must multiply to number inside the old cell
     // Start at (1,1), end is (M,N)
 
-    static class Node {
-        int cell;
-        int product;
+    static class Coord {
+        int x;
+        int y;
 
-        public Node(int r, int c, int cell) {
-            super();
-            this.cell = cell;
-            this.product = r * c;
+        public Coord(int x, int y){
+            this.x=x;
+            this.y=y;
         }
+    }
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    //initialize tokenizer
+    static StringTokenizer st;
+
+    //creates callable function for input
+    static int nextInt() throws IOException {
+        //while the tokenizer does not return a null value or has no more tokens trim
+        while (st == null || !st.hasMoreTokens()) {
+            st = new StringTokenizer(br.readLine().trim());
+        }
+        //return integer.parseInt(val)
+        return Integer.parseInt(st.nextToken());
     }
 
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int m = Integer.parseInt(br.readLine()); //num of cells in row
-        int n = Integer.parseInt(br.readLine()); // num of cells in column
-        LinkedList<Node> room = new LinkedList<>();
-        String line;
-        String[] tempArr;
-        for (int row = 0; row < m; row++) {
-            line = br.readLine();
-            tempArr = line.split(" ");
-            for(int col = 0; col < n; col++) {
-                room.add(new Node(row+1, col+1, Integer.parseInt(tempArr[col]))) ;
-            }
-            // Finished loading input into 2D array
+        int cell;
+        int m = nextInt(); //num of cells in row
+        int n = nextInt(); // num of cells in column
+        ArrayList<Coord>[] room = new ArrayList[(m+1)*(n+1)];
+        for (int i=0; i< room.length; i++) {
+            room[i] = new ArrayList<>();
         }
-        System.out.println(findPathFromEnd(room, room.getLast(), room.getFirst()));
-        br.close();
-    }
-
-    private static String findPathFromEnd(LinkedList<Node> room, Node source, Node destination) {
-        // Write a queue, if not the final element put all its children behind in the queue
-        LinkedList<Node> queue = new LinkedList<>();
-        queue.add(source);
-        Node node;
-        while (!queue.isEmpty()) {
-            node = queue.remove(); //removes head of linkedlist
-            if (node.product == destination.product) {
-                return "yes";
-            }
-            room.remove(node);
-            for (Node cell: room) {
-                if (node.product == cell.cell) {
-                    queue.add(cell);
+        //input values into array
+        for (int row = 1; row < m+1; row++) {
+            for (int col = 1; col < n+1; col++) {
+                //nextInt() is the inputted grid
+                //the node is the coordinates
+                cell = nextInt();
+                if (cell <= m*n) {
+                    room[cell].add(new Coord(row, col));
                 }
             }
         }
-        return "no";
+        boolean [][] visited = new boolean[m+1][n+1];
+        visited[m][n] = true;
+        LinkedList<Coord> queue = new LinkedList<>();
+        queue.add(new Coord(m, n));
+        while (!queue.isEmpty()) {
+            // Gets the head value and deletes it from queue
+            Coord cur = queue.poll();
+            for (Coord c: room[cur.x*cur.y]) {
+                if(!visited[c.x][c.y]){
+                    visited[c.x][c.y]=true;
+                    queue.add(c);
+                    //if the coordinates are both 1, that means they have successfully escaped
+                    if(c.x==1 && c.y==1){
+                        System.out.println("yes");
+                        return;
+                    }
+                }
+            }
+        }
+        System.out.println("no");
     }
 
 }
